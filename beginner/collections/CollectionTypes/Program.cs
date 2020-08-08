fe1102384bf6ba85744291ce1a1e31a627a73b3f
JanMichaelSe <janm.montalvo@gmail.com>
2020-08-07 23:26:45 -0400

    moved files and added section 2 of collections

---
+++ beginner/collections/CollectionTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CollectionTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            Console.Write("What day is it-->"); `
+            int iDay = int.Parse(Console.ReadLine());
+
+            string chosenDay = daysOfWeek[iDay - 1];
+            Console.WriteLine($"That day is {chosenDay}");
+        }
+    }
+}
